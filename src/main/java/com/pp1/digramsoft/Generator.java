20a55f566f8215c2d0754ed75cbd189d5c794be1
our0boros <863166709@qq.com>
2023-10-24 16:52:04 +1100

    [UPDATE] right side

--- src/main/java/com/pp1/digramsoft/Generator.java
+++ src/main/java/com/pp1/digramsoft/Generator.java
@@ -4,8 +4,11 @@ import javafx.event.ActionEvent;
 import javafx.event.Event;
 import javafx.event.EventHandler;
 import javafx.scene.Group;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Region;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
@@ -17,13 +20,12 @@ import java.util.ArrayList;
 public class Generator extends Group {
     public Button button;
     public TextField[] textFields;
-    public EventHandler<ActionEvent>[] eventEventHandler;
     public EntityType entityType;
-    public Circle[] colorSelect;
+    public Node[] colorSelect;
     public Group colorSelector;
     private boolean isColorSelectorVisable = false;
 
-    public Generator(double x, double y, String buttonText, EntityType entityType, VBox stakeholders, Group root) {
+    public Generator(double x, double y, String buttonText, EntityType entityType, VBox toShow, Group root) {
         this.entityType = entityType;
         this.colorSelector = new Group();
         // color selector
@@ -46,7 +48,7 @@ public class Generator extends Group {
         for (int idx = 0; idx < Stakeholder.StakeholderColor.values().length; idx++) {
             Circle color = new Circle(60 * (idx + 0.5), 30, 25, Stakeholder.StakeholderColor.values()[idx].getColor());
             color.setOnMouseClicked(event -> {
-                this.colorSelect[0].setFill(color.getFill());
+                ((Circle) this.colorSelect[0]).setFill(color.getFill());
                 this.isColorSelectorVisable = !this.isColorSelectorVisable;
                 this.colorSelector.setVisible(this.isColorSelectorVisable);
             });
@@ -71,11 +73,9 @@ public class Generator extends Group {
             };
             this.textFields[0].setMinWidth(240);
             this.textFields[0].setMinHeight(40);
-            this.eventEventHandler = new EventHandler[] {
-                event -> System.out.println("[Generator] input text: " + textFields[0].getText())
-            };
+
             // colorSelect
-            this.colorSelect = new Circle[] {
+            this.colorSelect = new Node[] {
                 new Circle(210, 75, 25)
             };
             this.colorSelect[0].setOnMouseClicked(event -> {
@@ -92,10 +92,10 @@ public class Generator extends Group {
             this.button.setOnAction(event -> {
                 System.out.println("[Generator] input text: " + textFields[0].getText());
 
-                Stakeholder stakeholder = new Stakeholder(this.textFields[0].getText(), (Color) this.colorSelect[0].getFill(), true, root);
+                Stakeholder stakeholder = new Stakeholder(this.textFields[0].getText(), (Color) ((Circle) this.colorSelect[0]).getFill(), true, root);
                 stakeholder.setLayoutX(10);
                 stakeholder.setLayoutY(10);
-                stakeholders.getChildren().add(stakeholder);
+                toShow.getChildren().add(stakeholder);
 //                root.getChildren().add(stakeholder);
 
                 // reset
@@ -109,6 +109,40 @@ public class Generator extends Group {
 
         // STAKEHOLDER MAP
         if (this.entityType == EntityType.STAKEHOLDER_MAP) {
+            /*        10
+                +-----+-------+
+                |50x50| 180x50|
+                +-----+-------+10
+                |  240x40     |
+                +-------------+10
+                |  240x40     |
+                +-------------+
+             */
+            // colorSelect
+            this.colorSelect = new Node[] {
+                    new ImageView(new Image("com/pp1/digramsoft/assets/colorCircle.png", 50, 50, false, false)) // 210, 75, 25
+            };
+            this.colorSelect[0].setOnMouseClicked(event -> {
+                System.out.println("[Generator] Open color selector");
+
+                this.isColorSelectorVisable = !this.isColorSelectorVisable;
+                this.colorSelector.setLayoutX(210);
+                this.colorSelector.setLayoutY(15);
+                this.colorSelector.setVisible(isColorSelectorVisable);
+                this.colorSelector.toFront();
+            });
+
+            // TextFields
+            this.textFields = new TextField[] {
+                    new TextField(),  // x interest
+                    new TextField()   // y influence
+            };
+            this.textFields[0].setMinWidth(240);
+            this.textFields[0].setMinHeight(40);
+            this.textFields[1].setMinWidth(240);
+            this.textFields[1].setMinHeight(40);
+
+
 
         }
 
