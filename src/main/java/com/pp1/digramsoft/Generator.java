55077e452703e696a3f6ae2969c30a667bee0915
our0boros <863166709@qq.com>
2023-10-24 12:19:49 +1100

    [UPDATE] Generator for Stakeholder

--- src/main/java/com/pp1/digramsoft/Generator.java
+++ src/main/java/com/pp1/digramsoft/Generator.java
@@ -1,11 +1,57 @@
 package com.pp1.digramsoft;
 
+import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
 
 public class Generator extends Group {
     public Button button;
     public TextField[] textFields;
-    
+    public EventHandler<ActionEvent>[] eventEventHandler;
+    public EntityType entityType;
+    public Circle[] colorSelect;
+
+    public Generator(double x, double y, String buttonText, EntityType entityType) {
+        this.entityType = entityType;
+
+        if (this.entityType == EntityType.STAKEHOLDER) {
+            // TextFields
+            this.textFields = new TextField[] {
+                new TextField()
+            };
+            this.textFields[0].setMinWidth(240);
+            this.textFields[0].setMinHeight(30);
+            this.eventEventHandler = new EventHandler[] {
+                event -> System.out.println("[Generator] input text: " + textFields[0].getText())
+            };
+            // colorSelect
+            this.colorSelect = new Circle[] {
+                new Circle()
+            };
+            // button
+            this.button = new Button(buttonText);
+            this.button.setOnAction(event -> {
+                System.out.println("[Generator] input text: " + textFields[0].getText());
+            });
+            this.button.setMinWidth(180);
+            this.button.setMinHeight(30);
+            this.button.setLayoutX(0);
+            this.button.setLayoutY(0);
+
+        }
+
+
+
+        // INIT
+        this.getChildren().add(this.button);
+        this.getChildren().addAll(this.textFields);
+        this.getChildren().addAll(this.colorSelect);
+        this.setLayoutX(x);
+        this.setLayoutY(y);
+    }
 }
--- src/main/java/com/pp1/digramsoft/HelloApplication.java
+++ src/main/java/com/pp1/digramsoft/HelloApplication.java
@@ -6,6 +6,7 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Polygon;
+import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -30,120 +31,17 @@ public class HelloApplication extends Application {
     private final Group root = new Group();
     private static final int WINDOW_WIDTH = 1280;
     private static final int WINDOW_HEIGHT = 720;
-    Triangle highlighted = null;
-    ArrayList<Triangle> triangles = new ArrayList<>();
-    Triangle findNearestTriangle(double x, double y) {
-//        System.out.println(triangles.get(0));
-        int minDistanceTriIdx = -1;
-        double minDistance = Double.POSITIVE_INFINITY;
-        for (int triIdx = 0; triIdx < triangles.size(); triIdx++) {
-            System.out.println(triangles.get(triIdx).x + " " + triangles.get(triIdx).x);
-            if (minDistance > triangles.get(triIdx).getDistance(x, y)) {
-                minDistance = triangles.get(triIdx).getDistance(x, y);
-                minDistanceTriIdx = triIdx;
-            }
-        }
-        if (minDistanceTriIdx == -1) return null;
-        return triangles.get(minDistanceTriIdx);
-    }
-    void highlightedNearestTriangle(double x, double y) {
-        if (highlighted != null) highlighted.setFill(Color.LIGHTGRAY);
-        highlighted = findNearestTriangle(x, y);
-        highlighted.setFill(Color.GREEN);
-    }
-
-    static class Triangle extends Polygon {
-        double x;
-        double y;
-        double side;
-        Triangle(double x, double y, double side) {
-            this.x = x;
-            this.y = y;
-            this.side = side;
-            updateTriangle();
-        }
-
-        private void updateTriangle() {
-            double midLine = Math.sqrt((side * side) - (side * side / 4)) / 2;
-            this.getPoints().addAll(0.0, -midLine,
-                    side / 2, midLine,
-                    -side / 2, midLine);
-            this.setLayoutX(x);
-            this.setLayoutY(y);
-        }
-
-        private double distance(double x, double y) {
-            double distanceSquare = (this.x - x)*(this.x - x) + (this.y - y)*(this.y - y);
-            return Math.sqrt(distanceSquare);
-        }
-
-        public double getDistance(double x, double y) {
-            return distance(x, y);
-        }
-    }
-
-    class DraggableTriangle extends Triangle {
-        private double mousex;
-        private double mousey;
-        DraggableTriangle(double x, double y, double side) {
-            super(x, y, side);
-            this.setOnMousePressed(event -> {
-                mousex = event.getSceneX();
-                mousey = event.getSceneY();
-//                System.out.println("mouseX/mouseY " + mousex + " " +mousey);
-
-                this.toFront();
-            });
-
-            this.setOnMouseDragged(event -> {
-                double deltaX = event.getSceneX() - mousex;
-                double deltaY = event.getSceneY() - mousey;
-//                System.out.println(this.x + " " + this.y + "\n" + deltaX + " " + deltaY + "\n");
-                this.setLayoutX(this.getLayoutX() + deltaX);
-                this.setLayoutY(this.getLayoutY() + deltaY);
-                mousex = event.getSceneX();
-                mousey = event.getSceneY();
-                highlightedNearestTriangle(event.getSceneX(), event.getSceneY());
-            });
-
-            this.setOnMouseReleased(event -> {
-                System.out.println("Highlighted rotate:" + highlighted.getRotate());
-                this.setRotate(highlighted.getRotate());
-                this.setLayoutX(highlighted.getLayoutX());
-                this.setLayoutY(highlighted.getLayoutY());
-            });
-        }
-    }
-
-
+    private static final int LEFT_WINDOW_WIDTH = 280;
+    private static final int LEFT_WINDOW_HEIGHT = 720;
     @Override
     public void start(Stage stage) throws Exception {
         Scene scene = new Scene(this.root, WINDOW_WIDTH, WINDOW_HEIGHT);
+        Rectangle leftWindow = new Rectangle(0, 0, LEFT_WINDOW_WIDTH, LEFT_WINDOW_HEIGHT);
+        leftWindow.setFill(Color.GRAY);
+        root.getChildren().add(leftWindow);
 
-//        Polygon tri = new Polygon();
-//        tri.getPoints().addAll(0.0, -86.6,
-//                100.0, 86.6,
-//                -100.0, 86.6);
-//        tri.setLayoutX((double) WINDOW_WIDTH /2);
-//        tri.setLayoutY((double) WINDOW_HEIGHT /2);
-//        Triangle tri = new Triangle((double) WINDOW_WIDTH / 2, (double) WINDOW_HEIGHT / 2, 200);
-//        tri.setFill(Color.LIGHTGRAY);
-//        this.root.getChildren().add(tri);
-//
-//
-//        for (int row = 0; row < 3; row++) {
-//            for (int col = 0; col < 5; col++) {
-//                Triangle grid = new Triangle((col + 1)*100, (row + 1) * 173.2 - 86.6, 196);
-//                grid.setFill(Color.LIGHTGRAY);
-//                if (col % 2 == 1) grid.setRotate(180);
-//                triangles.add(grid);
-//            }
-//        }
-//        this.root.getChildren().addAll(triangles);
-//
-//        DraggableTriangle dragTri = new DraggableTriangle((double) WINDOW_WIDTH / 2, (double) WINDOW_HEIGHT / 2, 200);
-//        dragTri.setFill(Color.RED);
-//        this.root.getChildren().add(dragTri);
+        Generator stakeholderGenerator = new Generator(10, 600, "Generate", EntityType.STAKEHOLDER);
+        root.getChildren().add(stakeholderGenerator);
 
         stage.setScene(scene);
         stage.show();
