f82f0404762d7d4febdff2628f2af952951d6dfa
Xing Chen <u7725171@anu.edu.au>
2023-10-24 19:10:19 +1100

    [Xing Chen][Coding]Added an icon in the stakeholder to hide/show it.

--- src/main/java/com/pp1/digramsoft/Stakeholder.java
+++ src/main/java/com/pp1/digramsoft/Stakeholder.java
@@ -2,6 +2,8 @@ package com.pp1.digramsoft;
 
 
 import javafx.scene.Group;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.paint.Color;
 import javafx.scene.shape.Circle;
 import javafx.scene.text.Text;
@@ -15,6 +17,9 @@ public class Stakeholder extends Group {
     private double circleRadius = 20;
     private Text text;
     private double textSize = 20;
+    private Image openEyeImage = new Image("file:src/main/java/com/pp1/digramsoft/assets/OpenEye.png");
+    private Image closedEyeImage = new Image("file:src/main/java/com/pp1/digramsoft/assets/ClosedEye.png");
+    private ImageView displayIcon;
     // a copy of itself to stay at the original place
     public Group draggablePart = new Group();
     public Stakeholder selfCopy;
@@ -55,21 +60,43 @@ public class Stakeholder extends Group {
         this.circle = new Circle(circleRadius);
         this.circle.setFill(this.color);
         this.draggablePart.getChildren().add(this.circle);
-        this.circle.setLayoutX(circleRadius);
+        this.circle.setLayoutX(circleRadius * 3.5);
         this.circle.setLayoutY(circleRadius);
 
         // generate the text
         this.text = new Text(this.name);
         this.text.setStyle("-fx-font-color: black; -fx-font-size: " + textSize + "px; -fx-font-weight: bold;");
         this.draggablePart.getChildren().add(this.text);
-        this.text.setLayoutX(2 * circleRadius + textSize);
+        this.text.setLayoutX(4.5 * circleRadius + textSize);
         this.text.setLayoutY(circleRadius * 5 / 4);
 
-        // the copy of itself, cannot be dragged, no need to copy again
         if (needCopy) {
+            // the copy of itself, cannot be dragged, no need to copy again
             this.selfCopy = new Stakeholder(this.name, this.color, false, root);
             this.selfCopy.setDraggable(false);
             this.getChildren().add(this.selfCopy);
+            // for the original icon, add a display button
+            this.displayIcon = new ImageView(openEyeImage);
+            this.displayIcon.setFitHeight(circleRadius * 1.5);
+            this.displayIcon.setFitWidth(circleRadius * 3);
+            this.displayIcon.setLayoutY(circleRadius * 0.3);
+            this.getChildren().add(displayIcon);
+            /* if clicked, then hide/display the draggablePart,
+             when hiding, make it not draggable;
+             otherwise, make it draggable */
+            this.displayIcon.setOnMouseClicked(event -> {
+                System.out.println("display icon clicked");
+                if (this.displayIcon.getImage() == openEyeImage) {
+                    this.displayIcon.setImage(closedEyeImage);
+                    this.draggablePart.setVisible(false);
+                    this.isDraggable = false;
+                }
+                else {
+                    this.displayIcon.setImage(openEyeImage);
+                    this.draggablePart.setVisible(true);
+                    this.isDraggable = true;
+                }
+            });
         }
 
         // set it draggable
@@ -104,16 +131,23 @@ public class Stakeholder extends Group {
      * @param root the stage root
      */
     public void addDraggableInScreen(double mouseX, double mouseY, Group root){
-        System.out.println("now the stakeholder " + this + "has children " + this.getChildren());
+//        System.out.println("now the stakeholder " + this + "has children " + this.getChildren());
         if (this.getChildren().contains(draggablePart)) {
             this.getChildren().remove(draggablePart);
             if (!root.getChildren().contains(draggablePart)) {
                 root.getChildren().add(draggablePart);
                 // move the draggable part to the location of mouse
-                draggablePart.setLayoutX(mouseX - circleRadius);
+                //System.out.println("draggablePart x: " + draggablePart.getLayoutX() + " mouseX: " + mouseX);
+                //System.out.println("draggablePart y: " + draggablePart.getLayoutY() + " mouseY: " + mouseY);
+                draggablePart.setLayoutX(mouseX - circleRadius * 3.5);
                 draggablePart.setLayoutY(mouseY - circleRadius);
+                //System.out.println("draggablePart x: " + draggablePart.getLayoutX());
+                //System.out.println("draggablePart y: " + draggablePart.getLayoutY());
             }
         }
+    }
+    public void setDragInvisible(){
+
     }
     public void setDraggable(boolean isDraggable){this.isDraggable = isDraggable;}
     public Stakeholder getSelfCopy(){return selfCopy;}
