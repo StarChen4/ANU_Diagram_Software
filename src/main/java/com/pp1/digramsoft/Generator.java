497ca4c7865c80e539ba997f44aba268d19bc190
our0boros <863166709@qq.com>
2023-10-24 11:43:04 +1100

    [UPDATE] add EntityType enum

---
+++ src/main/java/com/pp1/digramsoft/Generator.java
@@ -0,0 +1,11 @@
+package com.pp1.digramsoft;
+
+import javafx.scene.Group;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Generator extends Group {
+    public Button button;
+    public TextField[] textFields;
+    
+}
